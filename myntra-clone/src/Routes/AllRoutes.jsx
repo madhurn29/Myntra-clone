c9133385b8e626868b6888ec7ddc4d356a24b38f
Madhur Nadamwar <112754729+madhurn29@users.noreply.github.com>
2023-02-22 18:50:02 +0530

    routes added

---
+++ myntra-clone/src/Routes/AllRoutes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Admin from "../Pages/admin";
+
+function AllRoutes() {
+  return (
+    <Routes>
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  );
+}
+
+export default AllRoutes;
