8f9a2b6da5a5d4677f777259dcbd8179a7f0519c
saikiran12040 <saikirangen@gmail.com>
2023-02-23 12:41:52 +0530

    added files

---
+++ myntra-clone/src/Components/CartComponents/CartNavbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+
+
+const CartNavbar = () => {
+    const navigate=useNavigate()
+  return (
+    <>
+    <Box w="full">
+
+    </Box>
+    </>
+  )
+}
+
+export default CartNavbar
